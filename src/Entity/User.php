d306666f019a7fa22842784f11b728e01d118786
Maciej Iwan <64867638+MaciejIwan@users.noreply.github.com>
2023-04-18 03:06:12 +0200

    update User Entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,6 +6,7 @@ use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
@@ -16,17 +17,63 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column]
     private ?int $id = null;
 
+
+    #[Assert\NotBlank]
+    #[Assert\Length(min: 2, max: 50)]
+    #[ORM\Column(length: 50)]
+    private string $firstName;
+
+    #[Assert\NotBlank]
+    #[Assert\Length(min: 2, max: 100)]
+    #[ORM\Column(length: 100)]
+    private string $lastName;
+
+    #[Assert\NotBlank]
     #[ORM\Column(length: 180, unique: true)]
     private ?string $email = null;
+
+    #[Assert\NotBlank]
+    #[Assert\Length(min: 7, max: 250)]
+    #[ORM\Column(length: 250)]
+    #[Assert\Regex(
+        pattern: '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*(_|[^\w])).+$/',
+        message: 'Password must contain at least one uppercase letter, one lowercase letter, one number and one special character')
+    ]
+    private ?string $password = null;
 
     #[ORM\Column]
     private array $roles = [];
 
+
+    public function getLastName(): string
+    {
+        return $this->lastName;
+    }
+
     /**
-     * @var string The hashed password
+     * @param mixed $lastName
+     * @return User
      */
-    #[ORM\Column]
-    private ?string $password = null;
+    public function setLastName($lastName): self
+    {
+        $this->lastName = $lastName;
+        return $this;
+    }
+
+    public function getFirstName(): string
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @param mixed $firstName
+     * @return User
+     */
+    public function setFirstName($firstName): self
+    {
+        $this->firstName = $firstName;
+        return $this;
+    }
 
     public function getId(): ?int
     {
@@ -52,7 +99,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     public function getUserIdentifier(): string
     {
-        return (string) $this->email;
+        return (string)$this->email;
     }
 
     /**
